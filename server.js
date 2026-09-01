63c9684cf6b1c18cf666cdf5a2fab34940d0f7a7
Pedro Souza <pedro.oliveira8@edu.fecap.br>
2026-09-01 16:50:35 -0300

    parte 2 2 2 2 2
    em execução

--- server.js
+++ server.js
@@ -192,6 +192,7 @@ async function fetchOSMData(south, west, north, east) {
 
   return await response.json();
 }
+
 function processOSMData(data) {
   const roads = [];
   const buildings = [];
@@ -294,99 +295,6 @@ const server = http.createServer(async (req, res) => {
       );
   
       const mapData = processOSMData(osmData);
-      function createGraphFromRoads(roads) {
-        const nodes = [];
-        const edges = [];
-      
-        const nodeMap = new Map();
-      
-        function getNodeId(lat, lng) {
-          return `node_${lat.toFixed(7)}_${lng.toFixed(7)}`;
-        }
-      
-        function getOrCreateNode(lat, lng) {
-          const id = getNodeId(lat, lng);
-      
-          if (!nodeMap.has(id)) {
-            const node = {
-              id,
-              lat,
-              lng,
-              type: "road"
-            };
-      
-            nodeMap.set(id, node);
-            nodes.push(node);
-          }
-      
-          return nodeMap.get(id);
-        }
-      
-        function distanceBetween(a, b) {
-          const R = 6371000;
-      
-          const lat1 = a.lat * Math.PI / 180;
-          const lat2 = b.lat * Math.PI / 180;
-      
-          const dLat = (b.lat - a.lat) * Math.PI / 180;
-          const dLng = (b.lng - a.lng) * Math.PI / 180;
-      
-          const h =
-            Math.sin(dLat / 2) ** 2 +
-            Math.cos(lat1) *
-            Math.cos(lat2) *
-            Math.sin(dLng / 2) ** 2;
-      
-          return 2 * R * Math.asin(Math.sqrt(h));
-        }
-      
-        for (const road of roads) {
-      
-          if (!road.geometry || road.geometry.length < 2) {
-            continue;
-          }
-      
-          for (let i = 0; i < road.geometry.length - 1; i++) {
-      
-            const pointA = road.geometry[i];
-            const pointB = road.geometry[i + 1];
-      
-            const nodeA = getOrCreateNode(
-              pointA.lat,
-              pointA.lon
-            );
-      
-            const nodeB = getOrCreateNode(
-              pointB.lat,
-              pointB.lon
-            );
-      
-            const weight = distanceBetween(
-              nodeA,
-              nodeB
-            );
-      
-            edges.push({
-              from: nodeA.id,
-              to: nodeB.id,
-              weight
-            });
-      
-            if (road.type !== "motorway") {
-              edges.push({
-                from: nodeB.id,
-                to: nodeA.id,
-                weight
-              });
-            }
-          }
-        }
-      
-        return {
-          nodes,
-          edges
-        };
-      }
       res.writeHead(200, {
         'Content-Type': 'application/json'
       });
@@ -529,4 +437,4 @@ const server = http.createServer(async (req, res) => {
 server.listen(PORT, () => {
   console.log(`[SmartEvac] Servidor rodando em http://localhost:${PORT}`);
 });
-}
+
